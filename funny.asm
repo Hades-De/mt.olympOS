a684bf2fd574f8cb3aaee5c0c4c74a72373fb3f1
Hades-De <154917509+Hades-De@users.noreply.github.com>
2023-12-26 18:56:18 +0100

    file names will be changed later.
    file names are like this due to personal testing. it'll be updated as i go along.

---
+++ funny.asm
@@ -0,0 +1,5 @@
+[org 0x7E00]  
+
+    mov ah, 0x0E  
+    mov al, 'T'   
+    int 0x10
---
+++ test.asm
@@ -0,0 +1,29 @@
+org  0x7C00
+clc
+
+SegmentedReg:
+    mov ax, 0x0000
+    mov ds, ax
+    mov ax, 0x7E00
+    mov es, ax
+
+    mov ah, 0x0E  
+    mov al, 'T'   
+    int 0x10
+
+
+DiskLoad:
+    cld
+    mov dl, 0x80    
+    mov ah, 0x02
+    mov al, 8
+    mov ch, 0x00
+    mov dh, 0x00
+    mov cl, 0x02
+    mov bx, 0x7E00
+    int 0x13
+    jc DiskLoad
+    jnc 0x7E00
+
+times 510-($-$$) db 0
+db 0x55, 0xaa
