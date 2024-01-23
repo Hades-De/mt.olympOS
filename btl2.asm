cbc4e1ec769db26594b29426f4716bbe3df7e9ce
Hades-De <154917509+Hades-De@users.noreply.github.com>
2024-01-23 13:51:39 +0100

    Update btl2.asm

--- btl2.asm
+++ btl2.asm
@@ -1,18 +1,27 @@
-[org 0x7E00]  
+org 0x7E00
+
     mov ah, 0x0E  
     mov al, 'T'   
     int 0x10
+
+
+ckinput:
+    push eax
+    cmp cl, 0x32 ;;keymap should be put here + add the function to move up and down MOVES IT DOWN 
+    je print_From_Kb
+    cmp cl, 0x13 ;;enter current chosen kernel
+    pop eax
+    iret
 
 keyboardPs2:
     push eax        ;;so it could return whenever
     in al, 60h
     mov al, 20h
     out 20h, al
+    mov cl, al
     pop eax
     iret           ;;returns to whatever it was doing 
-
-                   ;;add some check for chars that could do mutliple things I.E choose kernel 
-
+                   ;;add some check for chars that could do mutliple things I.E choose kernel (check where theres no 00's or if its user specific)
 print_From_Kb:
     push eax
     mov al, 60h
