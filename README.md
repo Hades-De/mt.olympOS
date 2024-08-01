80535d6d4fe719dc0c77b4b3bdb81224934bb33b
mrtuxa <mrtuxa@nix2twink.gay>
2024-08-01 14:54:53 +0200

    reproducable build

---
+++ README.md
@@ -0,0 +1,3 @@
+# Building
+
+you just install the nix or lix package manager and run this command `nix-build`
--- default.nix
+++ default.nix
@@ -1,15 +1,13 @@
-with import <nixpkgs> {};
+with import <nixpkgs> { };
 
 stdenv.mkDerivation {
   name = "mt.olympOS";
-  src = ./.;
-
+  src = ./src;
   buildInputs = [ nasm ];
-
-  buildPhase = "nasm -f bin blt1.asm";
+  buildPhase = "nasm -f bin btl1.asm -o mt.olympOS.img";
 
   installPhase = ''
     mkdir -p $out/bin
-    cp main $out/bin/
+    cp mt.olympOS.img $out/bin
   '';
 }
