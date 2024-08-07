e88fc6d7ade30b49ec1d0a0c75d26b5e54b2ea14
mrtuxa <mrtuxa@nix2twink.gay>
2024-08-08 00:29:49 +0200

    updated ci

--- default.nix
+++ default.nix
@@ -3,12 +3,12 @@ with import <nixpkgs> { };
 stdenv.mkDerivation {
   name = "mt.olympOS";
   src = ./src;
-  buildInputs = [ nasm file ];
+  buildInputs = [ qemu_test nasm file ];
 
-  buildPhase = "nasm -f bin btl1.asm -o mt.olympOS.img";
+  buildPhase = "nasm -f bin blt1.asm -o blt1.bin && nasm -f bin blt2.asm -o blt2.bin && qemu-img create -f raw mt.olympOS.img 1M && dd if=boot1.bin of=mt.olympOS.img bs=512M count=1 conv=notrunc && dd if=boot2.bin of=mt.olympOS.img bs=512 seek=1 conv=notrunc";
 
   installPhase = ''
     mkdir -p $out/bin
-    cp mt.olympOS.img $out/bin
+    cp disk.img $out/bin
   '';
 }
