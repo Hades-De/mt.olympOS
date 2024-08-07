31c11d32f3d4a333b066846c5a9d20210d3fb4e0
tuxa <69870860+mrtuxa@users.noreply.github.com>
2024-08-08 00:38:20 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   src = ./src;
   buildInputs = [ qemu_test nasm file ];
 
-  buildPhase = "nasm -f bin src/blt1.asm -o blt1.bin && nasm -f bin src/blt2.asm -o blt2.bin && qemu-img create -f raw mt.olympOS.img 1M && dd if=boot1.bin of=mt.olympOS.img bs=512M count=1 conv=notrunc && dd if=boot2.bin of=mt.olympOS.img bs=512 seek=1 conv=notrunc";
+  buildPhase = "nasm -f bin src/blt1.asm -o boot1.bin && nasm -f bin src/blt2.asm -o boot2.bin && qemu-img create -f raw mt.olympOS.img 1M && dd if=boot1.bin of=mt.olympOS.img bs=512M count=1 conv=notrunc && dd if=boot2.bin of=mt.olympOS.img bs=512 seek=1 conv=notrunc";
 
   installPhase = ''
     mkdir -p $out/bin
